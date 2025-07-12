f1d76430396e9e9c476575767c77cebd268cc918
gustavo1902 <gustavomorais1009@gmail.com>
2025-07-12 19:43:47 -0300

    feat (backend): Método para comparar a senha fornecida com a senha hash no banco de dados

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,16 +15,26 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-    // Adiciona um campo para armazenar o hash da senha
+  // campos
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
 
+// Pré-salvamento: Hash da senha antes de salvar o usuário
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
 
+  const salt = await bcrypt.genSalt(10); 
+  this.password = await bcrypt.hash(this.password, salt); 
+  next();
+});
 
-
-
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
