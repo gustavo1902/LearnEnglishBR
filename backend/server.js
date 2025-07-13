3c38be50d1af4b55fa97ba96d34f9e92d6ff3a17
gustavo1902 <gustavomorais1009@gmail.com>
2025-07-13 11:48:16 -0300

    refactor (backend): app.use(cors)

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -19,7 +20,7 @@ app.get('/', (req, res) => {
   res.send('API está funcionando!');
 });
 
-const cors = require('cors');
+
 app.use(cors());
 
 app.use('/api/auth', authRoutes);
